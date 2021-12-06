e52cf32742a687cb5298d1c71c4c5be3769bb1b1
Marcus Sena <maarvins.19@gmail.com>
2021-12-06 20:18:08 -0300

    clear

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import {useEffect, useState} from "react"
 import {Button} from "./components/Button"
 import {MovieCard} from "./components/MovieCard"
 
-// import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
-
 import {api} from "./services/api"
 
 import "./styles/global.scss"
